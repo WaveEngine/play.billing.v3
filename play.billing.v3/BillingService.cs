e3ab2cfe96c473757cef7b4b49fdc8e0990d3eda
David Ávila <david.avila.membrives@gmail.com>
2015-05-05 18:04:59 +0200

    Replace Tcs.SetResult(...) with Tcs.TrySetResult(...)

--- play.billing.v3/BillingService.cs
+++ play.billing.v3/BillingService.cs
@@ -69,18 +69,18 @@ namespace play.billing.v3
 					if (!this.BindService(serviceIntent, this, Bind.AutoCreate))
 					{
 						Utils.LogError("Could not bind to the InAppBillingService.");
-						m_connectTcs.SetResult(false);
+						m_connectTcs.TrySetResult(false);
 					}
 				}
 				else
 				{
 					Utils.LogError("InAppBillingService is not available on this device.");
-					m_connectTcs.SetResult(false);
+					m_connectTcs.TrySetResult(false);
 				}
 			}
 			catch (System.Exception e)
 			{
-				m_connectTcs.SetResult(false);
+				m_connectTcs.TrySetResult(false);
 				Utils.LogError("Bind Exception: " + e.ToString());
 			}
 
@@ -135,7 +135,7 @@ namespace play.billing.v3
 				//Just indicates that the connect request has completed
 				// check PurchasesSupported to know whether or not v3 is supported, if successful
 				if (m_connectTcs != null)
-					m_connectTcs.SetResult(this.Connected);
+					m_connectTcs.TrySetResult(this.Connected);
 			}
 		}
 
@@ -234,7 +234,7 @@ namespace play.billing.v3
 			}
 			finally
 			{
-				buyReq.TCS.SetResult(new Response(responseCode));
+				buyReq.TCS.TrySetResult(new Response(responseCode));
 			}
 
 			return true;
